1739c7c8d4e56bc6dc2dd25d7821863151414f11
Yannick Foeillet <bzhtux@gmail.com>
2021-02-16 22:13:42 +0100

    [Fix] Travis yaml file

---
+++ docker.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+curl -X POST \
+    -H "Content-Type: application/json" \
+    -d "{\"source_type\": \"Tag\", \"source_name\": \"${TRAVIS_TAG}\"}" \
+    https://hub.docker.com/api/build/v1/source/6375f864-a015-4ec7-808e-0044240f81f7/trigger/f0175241-7846-4651-b66b-ea7280603026/call/
